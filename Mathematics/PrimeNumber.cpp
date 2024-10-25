8caa2753746d04755c4c3de4c47183ad9e9625dc
srm04072024 <s.r.m.20.3.2018@gmail.com>
2024-10-26 02:00:34 +0530

    Add Some Problems to Mathematics file

--- Mathematics/PrimeNumber.cpp
+++ Mathematics/PrimeNumber.cpp
@@ -2,13 +2,32 @@
 using namespace std;
 
 bool isPrime(int n){
-    for(int i =2 ; i<n ; i++){
-        if(n % i == 0){
+    if(n == 1) {return false;};
+////Optimized solution
+    if(n == 2 || n == 3) return true;
+    if(n % 2 == 0 || n % 3 == 0) return false;  
+    for(int i = 5 ; (i*i) <= n ; i= i+6){
+        if((n % i == 0)  || (n % (i+2) == 0)){
             return false;
         }
     }
+////Time Complexity =  O(n)
+    // for(int i =2 ; i<n ; i++){
+    //     if(n % i == 0){
+    //         return false;
+    //     }
+    // }
+
+////Time Complexity =  O(rootover(n))
+    // for(int i =2 ; (i*i)<n ; i++){
+    //     if(n % i == 0){
+    //         return false;
+    //     }
+    // }
     return true;
 }
+
+
 int main(){
     int n ;
     cin >> n;
